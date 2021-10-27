874ffc403533a31f5e115367ff8318fde9600513
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-10-27 02:41:40 -0700

    Added Snake and Ladder

---
+++ UC_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeandLadder
+{
+    class UC_1
+    {
+        public const int START = 0;
+        public void Game()
+        {
+            int playerPosition = START;
+            Console.WriteLine("Sinle Player Started At Positoin : " + playerPosition);
+        }
+    }
+}
---
+++ UC_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeandLadder
+{
+    class UC_2
+    {
+        public void DieRoll()
+        {
+            Random random = new Random();
+            int rollCheck = random.Next(1, 7);
+            Console.WriteLine("Number got on Die: " + rollCheck);
+        }
+    }
+}
